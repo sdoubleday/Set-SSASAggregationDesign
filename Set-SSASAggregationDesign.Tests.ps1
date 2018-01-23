5b3926838492a3e21da545d28f1a1db035bcbf3f
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2018-01-23 14:49:57 -0500

    Tests for FakePartition and Fake-SSASServer in place and passing. Tests for some of Set-SSASAggregationDesign in place. Need tests for refactored chunk of code to assign aggregation and run ProcessIndex (i.e. refactor, then mock out). Tests of the main function are to confirm control flow.

--- Set-SSASAggregationDesign.Tests.ps1
+++ Set-SSASAggregationDesign.Tests.ps1
@@ -5,8 +5,130 @@ $scriptBody = "using module $here\$sut"
 $script = [ScriptBlock]::Create($scriptBody)
 . $script
 
+
+Describe "CLASS FakePartition" {
+    
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).GetType().Name | Should Be 'FakePartition'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).GetType().Name | Should Be 'FakePartition' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).ID | SHOULD BE '1'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).ID | SHOULD BE '1' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Name | SHOULD BE 'PartitionName1'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Name | SHOULD BE 'PartitionName1' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).AggregationDesignID | Should Be 'ADID1'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).AggregationDesignID | Should Be 'ADID1' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).AggregationDesign.Aggregations.Count | Should Be '1'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).AggregationDesign.Aggregations.Count | Should Be '1' }
+
+    IT "[FakePartition]::New(3,'PartitionName3','',0).GetType().Name | Should Be 'FakePartition'" {
+        [FakePartition]::New(3,'PartitionName3','',0).GetType().Name | Should Be 'FakePartition' }
+
+    IT "[FakePartition]::New(3,'PartitionName3','',0).ID | SHOULD BE '3'" {
+        [FakePartition]::New(3,'PartitionName3','',0).ID | SHOULD BE '3' }
+
+    IT "[FakePartition]::New(3,'PartitionName3','',0).Name | SHOULD BE 'PartitionName3'" {
+        [FakePartition]::New(3,'PartitionName3','',0).Name | SHOULD BE 'PartitionName3' }
+
+    IT "[FakePartition]::New(3,'PartitionName3','',0).AggregationDesignID | Should Be ''" {
+        [FakePartition]::New(3,'PartitionName3','',0).AggregationDesignID | Should Be '' }
+
+    IT "[FakePartition]::New(3,'PartitionName3','',0).AggregationDesign.Aggregations.Count | Should Be '0'" {
+        [FakePartition]::New(3,'PartitionName3','',0).AggregationDesign.Aggregations.Count | Should Be '0' }
+
+    IT "Adding a ScriptMethod named Process to a FakePartition that just calls ProcessBob does not produce an error." {
+        $result = TRY { ( Add-Member -PassThru -InputObject $( [FakePartition]::New(3,'PartitionName3','',0) ) -MemberType ScriptMethod -Name 'Process' -Value {Param([String]$ProcessType) $this.ProcessBob($ProcessType)} ).Process('ProcessIndex') } catch {$error[0].Exception.GetBaseException().Message.ToString()}
+        $result | SHOULD BE $NULL }
+
+    IT "`$n =[FakePartition]::New(3,'PartitionName1','',0); `$n.Update(); `$n.AggregationDesign.Aggregations.Count | SHOULD BE '1'" {
+        $n =[FakePartition]::New(3,'PartitionName1','',0); $n.Update(); $n.AggregationDesign.Aggregations.Count | SHOULD BE '1' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns.Count | SHOULD BE '2'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns.Count | SHOULD BE '2' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[0].Name | SHOULD BE 'FakeAggregationDesignName1'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[0].Name | SHOULD BE 'FakeAggregationDesignName1' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[0].ID | SHOULD BE '7'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[0].ID | SHOULD BE '7' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[1].Name | SHOULD BE 'FakeAggregationDesignName2'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[1].Name | SHOULD BE 'FakeAggregationDesignName2' }
+
+    IT "[FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[1].ID | SHOULD BE '8'" {
+        [FakePartition]::New(1,'PartitionName1','ADID1',1).Parent.AggregationDesigns[1].ID | SHOULD BE '8' }
+
+
+} <#END Describe "CLASS FakePartition"#>
+
+DESCRIBE "Fake-SSASServer" {
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Name | SHOULD BE 'ImAServerISwear'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Name | SHOULD BE 'ImAServerISwear' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases.Count | SHOULD BE '3'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases.Count | SHOULD BE '3' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[0].Name | SHOULD BE 'Db1'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[0].Name | SHOULD BE 'Db1' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Name | SHOULD BE 'Db2'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Name | SHOULD BE 'Db2' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[0].Name | SHOULD BE 'Cube1'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[0].Name | SHOULD BE 'Cube1' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes.Count | SHOULD BE '3'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes.Count | SHOULD BE '3' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].Name | SHOULD BE 'Cube3'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].Name | SHOULD BE 'Cube3' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups.Count | SHOULD BE '3'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups.Count | SHOULD BE '3' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Name | SHOULD BE 'Mg2'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Name | SHOULD BE 'Mg2' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Partitions.Count | SHOULD BE '4'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Partitions.Count | SHOULD BE '4' }
+
+    IT "(Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Partitions[0].GetType().Name | SHOULD BE 'FakePartition'" {
+        (Fake-SSASServer -SSASInstance 'Whatever').Databases[1].Cubes[2].MeasureGroups[1].Partitions[0].GetType().Name | SHOULD BE 'FakePartition' }
+
+} <#END DESCRIBE "Fake-SSASServer"#>
+
 Describe "Set-SSASAggregationDesign" {
-    It "does something useful" {
-        $true | Should Be $false
+    BEFOREALL{
+        MOCK -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -MockWith {Fake-SSASServer -SSASInstance 'Whatever'} -Verifiable
+        MOCK -CommandName Get-SSASPartitionAggregationsProcessedCount -ModuleName .\Set-SSASAggregationDesign -MockWith {1} -Verifiable
     }
-}
+
+    It "Set-SSASAggregationDesign on one partition WITH aggregations: Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly" { 
+        Set-SSASAggregationDesign -SSASInstance 'ImNotAServer' -ssasDb 'ImNotADb' -CubeName 'Cube2' -MeasureGroupName 'Mg1' -PartitionName 'PartitionName2' 
+        Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly
+    }
+
+    It "Set-SSASAggregationDesign on one partition WITHOUT aggregations: Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly" { 
+        Set-SSASAggregationDesign -SSASInstance 'ImNotAServer' -ssasDb 'ImNotADb' -CubeName 'Cube2' -MeasureGroupName 'Mg1' -PartitionName 'PartitionName2' 
+        Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly
+    }
+
+    It "Set-SSASAggregationDesign on one partition WITH aggregations: Assert-MockCalled -CommandName Get-SSASPartitionAggregationsProcessedCount -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly" { 
+        Set-SSASAggregationDesign -SSASInstance 'ImNotAServer' -ssasDb 'ImNotADb' -CubeName 'Cube2' -MeasureGroupName 'Mg1' -PartitionName 'PartitionName3' 
+        Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly
+    }
+
+    It "Set-SSASAggregationDesign on one partition WITHOUT aggregations: Assert-MockCalled -CommandName Get-SSASPartitionAggregationsProcessedCount -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly" { 
+        Set-SSASAggregationDesign -SSASInstance 'ImNotAServer' -ssasDb 'ImNotADb' -CubeName 'Cube2' -MeasureGroupName 'Mg1' -PartitionName 'PartitionName3' 
+        Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly
+    }
+
+    It "Set-SSASAggregationDesign on a MeasureGroup of 4 partitions, half WITHOUT aggregations: Assert-MockCalled -CommandName Get-SSASPartitionAggregationsProcessedCount -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 1 -Exactly" { 
+        Set-SSASAggregationDesign -SSASInstance 'ImNotAServer' -ssasDb 'ImNotADb' -CubeName 'Cube2' -MeasureGroupName 'Mg1'
+        Assert-MockCalled -CommandName Get-SSASServer -ModuleName .\Set-SSASAggregationDesign -Scope It -Times 4 -Exactly
+    }
+
+}<#End Describe "Set-SSASAggregationDesign" #>
--- Set-SSASAggregationDesign.psm1
+++ Set-SSASAggregationDesign.psm1
@@ -1,5 +1,103 @@
 ﻿
+CLASS FakePartition {
+    [PSCustomObject]$Parent
+    [String]$ID
+    [String]$Name
+    [String]$AggregationDesignID
+    [PSCustomObject]$AggregationDesign
+    
+    FakePartition([String]$ID,[String]$Name,[String]$AggregationDesignID,[Int]$AggregationDesignAggregationsCount){
+        $PartitionParentAggregationDesigns = @()
+        $PartitionParentAggregationDesigns += [PSCustomObject]@{Name='FakeAggregationDesignName1';ID='7'}
+        $PartitionParentAggregationDesigns += [PSCustomObject]@{Name='FakeAggregationDesignName2';ID='8'}
 
+        $Aggregations = [PSCustomObject]@{Count=$AggregationDesignAggregationsCount}
+
+        $this.AggregationDesign = [PSCustomObject]@{Aggregations=$Aggregations}
+
+        $this.Parent = [PSCustomObject]@{AggregationDesigns=$PartitionParentAggregationDesigns}
+
+        $this.ID = $ID
+        $this.Name = $Name
+        $this.AggregationDesignID = $AggregationDesignID
+    }<#End Constructor#>
+
+    <#Process is a reserved word. Maybe there's a way to add it as a method, but just wrapping it using Add-member in the calling code works for my Pester Testing purposes.#>
+    [Void] ProcessBob($ProcessType) {
+        Start-sleep -Seconds 1 
+        Write-Verbose -Verbose "Running Fake .Process($ProcessType)" } <#End Method Process($ProcessType)#>
+
+    [Void] Update() {$this.AggregationDesign.Aggregations.Count += 1}
+
+}<#End Class FakePartition#>
+
+FUNCTION Fake-SSASServer {
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(Position=0,mandatory=$true)]
+        [string] $ssasInstance
+    )
+
+    'Ginning up something that pretends to be a SSAS Server for testing purposes...' | Write-Verbose -Verbose
+
+    $Partitions = @()
+    $Partitions += Add-Member -PassThru -InputObject $( [FakePartition]::New(1,'PartitionName1','ADID1',1) ) -MemberType ScriptMethod -Name 'Process' -Value {Param([String]$ProcessType) $this.ProcessBob($ProcessType)}
+    $Partitions += Add-Member -PassThru -InputObject $( [FakePartition]::New(2,'PartitionName2','ADID1',1) ) -MemberType ScriptMethod -Name 'Process' -Value {Param([String]$ProcessType) $this.ProcessBob($ProcessType)}
+    $Partitions += Add-Member -PassThru -InputObject $( [FakePartition]::New(3,'PartitionName3','',0) ) -MemberType ScriptMethod -Name 'Process' -Value {Param([String]$ProcessType) $this.ProcessBob($ProcessType)}
+    $Partitions += Add-Member -PassThru -InputObject $( [FakePartition]::New(4,'PartitionName4','',0) ) -MemberType ScriptMethod -Name 'Process' -Value {Param([String]$ProcessType) $this.ProcessBob($ProcessType)}
+    
+    
+    $MeasureGroups = @()
+    $MeasureGroups += [PSCustomObject]@{Name='MG1';Partitions=$Partitions}
+    $MeasureGroups += [PSCustomObject]@{Name='MG2';Partitions=$Partitions}
+    $MeasureGroups += [PSCustomObject]@{Name='MG3';Partitions=$Partitions}
+
+    $Cubes = @()
+    $Cubes += [PSCustomObject]@{Name='Cube1';MeasureGroups=$MeasureGroups}
+    $Cubes += [PSCustomObject]@{Name='Cube2';MeasureGroups=$MeasureGroups}
+    $Cubes += [PSCustomObject]@{Name='Cube3';MeasureGroups=$MeasureGroups}
+
+    $Databases = @()
+    $Databases += [PSCustomObject]@{Name='Db1';Cubes=$Cubes}
+    $Databases += [PSCustomObject]@{Name='Db2';Cubes=$Cubes}
+    $Databases += [PSCustomObject]@{Name='Db3';Cubes=$Cubes}
+
+    $SSASServer = [PSCustomObject]@{Name='ImAServerISwear';Databases=$Databases}
+
+    Return $SSASServer
+
+}<#End FUNCTION Fake-SSASServer#>
+
+
+<#These are easier to mock than having them in the main function.#>
+
+FUNCTION Get-SSASServer {
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(Position=0,mandatory=$true)]
+        [string] $ssasInstance
+    )
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices")
+$server = New-Object Microsoft.AnalysisServices.Server
+$server.connect($ssasInstance)
+} <#End FUNCTION Get-SSASServer#>
+
+FUNCTION Get-SSASPartitionAggregationsProcessedCount {
+PARAM($ssasInstance,$dbase,$cube,$mg,$partition)
+
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices.AdomdClient")
+ $con = new-object Microsoft.AnalysisServices.AdomdClient.AdomdConnection("datasource=$ssasInstance")
+ $con.Open()
+ $MDX="SELECT * FROM SystemRestrictSchema(`$system.discover_partition_stat, DATABASE_NAME = '$dbase',CUBE_NAME = '$cube', MEASURE_GROUP_NAME = '$mg', PARTITION_NAME = '$partition')"
+ $command = new-object Microsoft.AnalysisServices.AdomdClient.AdomdCommand($MDX, $con) 
+ $dataAdapter = new-object Microsoft.AnalysisServices.AdomdClient.AdomdDataAdapter($command)
+ $ds = new-object System.Data.DataSet
+ $dataAdapter.Fill($ds) > $null
+ $con.Close();
+ $dataExtract = $ds.tables[0].select("AGGREGATION_NAME IS NOT NULL")
+ 
+ Return $DataExtract.Count
+ } <#END Get-SSASPartitionAggregationsProcessedCount#>
 
 
 function Set-SSASAggregationDesign {
@@ -28,72 +126,6 @@ https://redphoenix.me/2014/11/11/setting-aggregation-designs-on-ssas-partitions-
  [switch] $Fix)
  
  
-[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices")
-$server = New-Object Microsoft.AnalysisServices.Server
-$server.connect($ssasInstance)
-$database=$server.databases
-$dbase=$database[$ssasdb]
- 
-$Cubes=New-object Microsoft.AnalysisServices.Cube
-$Cubes=$dbase.cubes
- 
-foreach ($cube in $cubes)
- {
- $Cube|select name,state,lastprocessed 
- foreach ($mg in $cube.MeasureGroups)
- {
- foreach ($partition in $mg.Partitions)
- {
- $totalAggCount = $partition.AggregationDesign.Aggregations.Count
- [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices.AdomdClient")
- $con = new-object Microsoft.AnalysisServices.AdomdClient.AdomdConnection("datasource=$ssasInstance")
- $con.Open()
- $MDX="SELECT * FROM SystemRestrictSchema(`$system.discover_partition_stat, DATABASE_NAME = '$dbase',CUBE_NAME = '$cube', MEASURE_GROUP_NAME = '$mg', PARTITION_NAME = '$partition')"
- $command = new-object Microsoft.AnalysisServices.AdomdClient.AdomdCommand($MDX, $con) 
- $dataAdapter = new-object Microsoft.AnalysisServices.AdomdClient.AdomdDataAdapter($command)
- $ds = new-object System.Data.DataSet
- $dataAdapter.Fill($ds) > $null
- $con.Close();
- $dataExtract = $ds.tables[0].select("AGGREGATION_NAME IS NOT NULL")
- $processedAggCount = $DataExtract.Count
- $totalAggCount = $partition.AggregationDesign.Aggregations.Count
- if($partition.AggregationDesignID.Length -lt 1)
- {
- "$($partition.Name) does not have an aggregation design applied"
- if ($fix.IsPresent)
- {
- if ( $partition.Parent.AggregationDesigns.Count -gt 0)
- {
- "$($partition.Name) assigning aggregation design $($partition.Parent.AggregationDesigns[$partition.Parent.AggregationDesigns.Count-1].ID)"
- $partition.AggregationDesignID = $partition.Parent.AggregationDesigns[$partition.Parent.AggregationDesigns.Count-1].ID
- $partition.Update()
- $totalAggCount = $partition.AggregationDesign.Aggregations.Count
- }
- else
- {"$($partition.Name) does not have a aggregation design created for it. Ignoring..."}
-  
- }
- if ($totalAggCount -ne $processedAggCount)
- {
-  
- "$($partition.Name) aggregation design $($partition.AggregationDesignID) has $processedAggCount of $totalAggCount processed "
- if($Fix.IsPresent)
- {
- if ( $partition.Parent.AggregationDesigns.Count -gt 0)
- {
- $date1=get-date
- "$($partition.Name) processing..."
- $partition.Process("ProcessIndexes")
- $date2=get-date
- "$($partition.Name) done. Processing took " + ($date2-$date1).Hours + " Hours, " + ($date2-$date1).Minutes + " Mins, " + ($date2-$date1).Seconds + " Secs "
- }
- }
- }
- } 
- }
- }
- }
-
 
 
 
